25c9dfa5810bb44abb49851ab016003dc2373337
Ali Azhari <aliazhari@gmail.com>
2026-09-14 14:24:45 -0400

    First program testing polymorphism

---
+++ 2026_09_14/Animal.java
@@ -0,0 +1,10 @@
+class Animal {
+  
+    public void makeSound() {
+        System.out.println("Animal Makes sound");
+
+    }
+    public void eat() {
+        System.out.println("Animals eats");
+    }
+}
---
+++ 2026_09_14/Dod.java
@@ -0,0 +1,11 @@
+public class Dod extends Animal {
+    
+    @Override
+    public void makeSound() {
+        System.out.println("Dog is barking");
+    }
+
+    public void bark() {
+        System.out.println("Dog is barking");
+    }
+}
